8bdcb33fe66e27e9d4b9f56ce5bc67684305a4f3
brigidamirella <brigidamirella@hotmail.com>
2022-07-14 18:23:24 -0300

    update

--- app/src/main/java/com/example/themovie/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/themovie/ui/activity/HomeActivity.kt
@@ -2,8 +2,13 @@ package com.example.themovie.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.NavHostFragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.themovie.R
 import com.example.themovie.api.MovieApiInterface
 import com.example.themovie.api.MovieApiService
 import com.example.themovie.databinding.ActivityMainBinding
@@ -11,17 +16,17 @@ import com.example.themovie.model.Movie
 import com.example.themovie.model.MovieResponse
 import com.example.themovie.ui.details.DetailActivity
 import com.example.themovie.ui.recyclerview.adapter.MovieAdapter
+import kotlinx.android.synthetic.main.activity_main.view.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class HomeActivity : AppCompatActivity() {
-
+    private var movie: Movie? = null
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
         binding.rvMoviesList.layoutManager = LinearLayoutManager(this)
         binding.rvMoviesList.setHasFixedSize(true)
         getMovieData { movies: List<Movie> ->
@@ -38,6 +43,28 @@ class HomeActivity : AppCompatActivity() {
         }
 
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.action_favorites -> {
+                Intent(this, FavoritesActivity::class.java)
+                    .apply {
+                        startActivity(this)
+                    }
+
+            }
+
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
+
 
     private fun getMovieData(callback: (List<Movie>) -> Unit) {
         val apiService = MovieApiService.getInstance().create(MovieApiInterface::class.java)
